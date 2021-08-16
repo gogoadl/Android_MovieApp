9e0d72a3617629e7432da7922ca786d72057c898
HyeonWoo Park <49335446+gogoadl@users.noreply.github.com>
2021-08-16 14:05:11 +0900

    2.2

---
+++ app/src/main/java/com/hyeonwoo/android_movie_app_mvvm/single_movie_details/SingleMovie.kt
@@ -0,0 +1,11 @@
+package com.hyeonwoo.android_movie_app_mvvm.single_movie_details
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SingleMovie : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_single_movie)
+    }
+}
